02dc9e7477470a13f04ef4bae833ec3d958e6f35
Guilherme Martins <Guilhermartins10@gmail.com>
2025-01-25 18:58:12 -0300

    Adicionando_Campo_para_limpar_Input

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ let BarraDeTexto = document.querySelector('#amigo');
 
 
 function adicionarAmigos(){
-    if (BarraDeTexto.value == '' ) {
+    if (BarraDeTexto.value === '' ) {
         alert("Escreva os nomes de seus amigos(a)"); //Se o valor da BarraDeTexto for igual á string vazia elerte está mensagem.
         return;
     } if (amigos.includes(BarraDeTexto.value)) { 
@@ -11,8 +11,11 @@ function adicionarAmigos(){
         return;
     } if (amigos.push(BarraDeTexto.value)) { //Adiciona o nome do amigo
         console.log(amigos);
+        BarraDeTexto.value = ""; //Limpando a barra de texto
         return;
-    } 
-
+    }
+    
+    
+    
 }
 
